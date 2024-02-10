fdb480276eb1fdddb5d4c3e7f63b5de0901f8002
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-02-11 02:01:54 +0900

    feat: lucide icon

--- app/matches/[matchId]/_components/type-challenge-form/submit-button.tsx
+++ app/matches/[matchId]/_components/type-challenge-form/submit-button.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { useFormStatus } from "react-dom";
+import { Sword } from "lucide-react";
 
 export const SubmitButton = () => {
   const { pending } = useFormStatus();
   return (
-    <Button type="submit" className="text-white px-2 w-fit">
+    <Button type="submit" className="text-white px-2 w-fit flex gap-x-1">
+      <Sword />
       {pending ? "確認中" : "提出する"}
     </Button>
   );

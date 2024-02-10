6691cd0d351313308d778c481905420b57a22a07
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-02-11 03:22:49 +0900

    feat: disabled追加

--- app/matches/[matchId]/_components/type-challenge-form/submit-button.tsx
+++ app/matches/[matchId]/_components/type-challenge-form/submit-button.tsx
@@ -5,7 +5,11 @@ import { Sword } from "lucide-react";
 export const SubmitButton = () => {
   const { pending } = useFormStatus();
   return (
-    <Button type="submit" className="text-white px-2 w-fit flex gap-x-1">
+    <Button
+      type="submit"
+      className="text-white px-2 w-fit flex gap-x-1"
+      disabled={pending}
+    >
       <Sword />
       {pending ? "確認中" : "提出する"}
     </Button>

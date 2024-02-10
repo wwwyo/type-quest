63a8b46fb3be65955b972b806e5719cc1017a5a0
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-02-11 03:23:30 +0900

    feat: prisma接続

--- app/matches/[matchId]/page.tsx
+++ app/matches/[matchId]/page.tsx
@@ -13,34 +13,74 @@ export default async function Page({
   const { matchId } = params;
   const { questId } = searchParams;
 
-  // prisma でmatchを取得
   const match = await prisma.match.findUnique({
-    select: { id: true, status: true },
+    select: {
+      id: true,
+      status: true,
+      playerOneId: true,
+      playerTwoId: true,
+      matchQuestions: {
+        select: {
+          id: true,
+          question: {
+            select: {
+              id: true,
+              description: true,
+              defaultCode: true,
+            },
+          },
+        },
+      },
+    },
     where: { id: +matchId },
   });
 
+  const isCurrentQuest = (id: number): boolean => id === Number(questId);
+  const currentQuest = match?.matchQuestions.find((quest) =>
+    isCurrentQuest(quest.id)
+  );
+
   return (
     <div className="grid grid-cols-2">
-      <ScrollArea className="h-[90vh]">
-        <div className="font-bold text-lg">クエスト</div>
-        <ul className="flex gap-x-2">
-          {["1", "2", "3"].map((id) => (
-            <Button
-              key={id}
-              variant={questId === id ? "default" : "outline"}
-              size="sm"
-              asChild
-            >
-              <a href={`?questId=${id}`}>{id}</a>
-            </Button>
-          ))}
-        </ul>
-      </ScrollArea>
-      <div className="grid h-full">
-        <TypeChallengeForm
-          questionId="1"
-          sutCode="type Condition<T, U> = T extends U ? T : unknown;"
-        />
+      <div>
+        {/* タブ */}
+        <div className="flex items-center gap-x-3 mt-7">
+          <div className="font-bold text-2xl">Quest</div>
+          <ul className="flex gap-x-2">
+            {match?.matchQuestions.map((quest, idx) => (
+              <Button
+                key={quest.id}
+                variant={isCurrentQuest(quest.id) ? "default" : "outline"}
+                size="icon"
+                asChild
+              >
+                <a href={`?questId=${quest.id}`}>{idx + 1}</a>
+              </Button>
+            ))}
+          </ul>
+        </div>
+
+        <ScrollArea className="h-[80vh] grid">
+          <div>
+            {currentQuest ? (
+              <div>
+                <h1 className="text-2xl font-bold">
+                  {currentQuest.question.description}
+                </h1>
+              </div>
+            ) : (
+              <div className="mt-10">Questを選択してね</div>
+            )}
+          </div>
+        </ScrollArea>
+      </div>
+      <div className="pt-10">
+        {currentQuest ? (
+          <TypeChallengeForm
+            questionId={currentQuest.id}
+            sutCode={currentQuest.question.defaultCode}
+          />
+        ) : null}
       </div>
     </div>
   );

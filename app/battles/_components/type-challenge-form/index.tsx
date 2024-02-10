69dbc93036e617dd82bfcba5691c7b1e094c4e82
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-02-11 00:39:41 +0900

    feat: editor読み込み

--- app/battles/_components/type-challenge-form/index.tsx
+++ app/battles/_components/type-challenge-form/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { checkType } from "../../_actions/check";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useFormState } from "react-dom";
 import { SubmitButton } from "./submit-button";
+import Editor from '@monaco-editor/react'
 
 type Props = {
   questionId: string;
@@ -11,10 +12,11 @@ type Props = {
 };
 
 export const TypeChallengeForm: FC<Props> = (defaultValues) => {
+  const [code, setCode] = useState(defaultValues.sutCode);
   const [state, formAction] = useFormState(checkType, { type: "initial" });
 
   return (
-    <form action={formAction} className="grid w-full">
+    <form action={formAction} className="grid w-full gap-y-5">
       {state.type === "error" ? (
         <div>
           {state.errors.questionId && (
@@ -31,10 +33,13 @@ export const TypeChallengeForm: FC<Props> = (defaultValues) => {
       ) : null}
       <input type="hidden" name="questionId" id={defaultValues.questionId} />
       <textarea
-        className="border p-4"
+        readOnly
+        className="hidden"
         name="sutCode"
-        defaultValue={defaultValues.sutCode}
+        value={code}
       />
+      {/* https://www.npmjs.com/package/@monaco-editor/react */}
+      <Editor className="rounded-md border-2 border-green-500" height="80vh" width="100%" defaultLanguage="typescript" value={code} onChange={(v) => v && setCode(v)} />
       <SubmitButton />
     </form>
   );

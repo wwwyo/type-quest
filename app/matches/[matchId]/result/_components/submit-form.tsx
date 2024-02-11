75e7201f67a2b9d2513c3ab11dbbc2fad06542ae
yuito.watanabe <w.ytmrm11811@gmail.com>
2024-02-11 13:12:29 +0900

    feat: 完成

--- app/matches/[matchId]/result/_components/submit-form.tsx
+++ app/matches/[matchId]/result/_components/submit-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { User } from "@prisma/client";
 import { follow } from "../_actions/follow";
@@ -7,14 +8,14 @@ export const SubmitForm = ({ enemy }: { enemy: User }) => {
   const [state, formAction] = useFormState(follow, { ok: false });
 
   return (
-    <div>
+    <div className="grid gap-y-2">
       <form action={formAction}>
         <input type="hidden" name="userId" value={enemy.qiitaId} />
         <Button className="w-fit">対戦相手とつながる</Button>
       </form>
 
       {state.ok && (
-        <div>
+        <div className="grid gap-y-2">
           <p>フォローしました</p>
           <Button variant="link" asChild>
             <a href="/">ホームに戻る</a>
